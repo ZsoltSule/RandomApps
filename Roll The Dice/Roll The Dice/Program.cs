2f060bad37c83272b538233da97133feb6bf6514
ZsoltSule <115541802+ZsoltSule@users.noreply.github.com>
2022-12-08 11:14:59 +0200

    Add files via upload

---
+++ Roll The Dice/Roll The Dice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random number = new Random();
+            int rollFirst = 0;
+            int rollSecond = 0;
+            int attempts = 0;
+
+            Console.WriteLine("Press enter to roll the dice.\n");
+
+            while (rollFirst != rollSecond || (rollFirst == 0 && rollSecond == 0))
+            {
+                Console.ReadKey();
+                rollFirst = number.Next(1, 7);
+                rollSecond = number.Next(1, 7);
+
+                Console.WriteLine($"You rolled {rollFirst} and {rollSecond}\n");
+                attempts++;
+            }
+
+            Console.WriteLine($"It took you {attempts} attempts to roll two of a kind");
+            Console.WriteLine($"You rolled {rollFirst} and {rollSecond}");
+        }
+    }
+}
